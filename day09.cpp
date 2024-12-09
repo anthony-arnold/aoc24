19482dfa7e2a2386bd4f2f2d4b71925b0df22a66
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2024-12-10 00:04:30 +1000

    Day 9

---
+++ day09.cpp
@@ -0,0 +1,139 @@
+#include "day09.hpp"
+#include <ranges>
+#include <vector>
+#include <algorithm>
+
+namespace aoc {
+
+constexpr auto chunked_v = std::views::chunk(day09_v, 2);
+constexpr size_t num_files_v = chunked_v.size();
+
+constexpr auto blocks() {
+   size_t id {};
+   size_t blk {};
+   std::array<std::array<size_t, 4>, num_files_v> chunks {};
+   for (auto [size, free] : chunked_v | std::views::transform([](auto p) {
+      return std::make_pair(
+         static_cast<size_t>(p[0] - '0'),
+         p[1] == '\n' ? 0 : static_cast<size_t>(p[1] - '0'));
+   }))
+   {
+      chunks[id++] = std::array<size_t, 4> { size, blk, blk+size, free };
+      blk += free + size;
+   }
+   return chunks;
+}
+
+constexpr auto blocks_v = blocks();
+
+constexpr size_t defrag() {
+   auto blk = blocks_v;
+   auto rit = blk.end() - 1;
+   auto it = blk.begin();
+
+   size_t csum {};
+   size_t loc = 0;
+   while (it != blk.end()) {
+      size_t id = static_cast<size_t>(std::distance(blk.begin(), it));
+      auto& [size, _0, _00, free] = *it;
+
+      size_t summa = (loc+size) * (loc+size-1) / 2 -
+         loc * (loc-1) / 2;
+      csum += summa * id;
+      loc += size;
+
+      while (rit > it && free > 0) {
+         auto& [rsize, _1, _11, _111] = *rit;
+
+         size_t rid = static_cast<size_t>(std::distance(blk.begin(), rit));
+         auto fill = std::min(free, rsize);
+
+         summa = (loc+fill) * (loc+fill-1) / 2 -
+            loc * (loc-1) / 2;
+         csum += summa * rid;
+         loc += fill;
+         free -= fill;
+         rsize -= fill;
+         if (rsize == 0) {
+            rit--;
+         }
+      }
+      it++;
+   }
+
+   return csum;
+}
+
+constexpr size_t min_size(auto&& r) {
+   return std::ranges::min(r, {}, [](auto p) {
+      return p[0];
+   })[0];
+}
+constexpr size_t max_free(auto&& r) {
+   return std::ranges::max(r, {}, [](auto p) {
+      return p[3];
+   })[3];
+}
+
+constexpr size_t compress() {
+   auto blk = blocks_v;
+   size_t csum {};
+
+   size_t min_size_ = min_size(blk);
+   size_t max_free_ = max_free(blk);
+   std::vector<decltype(blk.begin())> avail {};
+   for(auto it = blk.begin(); it != blk.end()-1; it++) {
+      if ((*it)[3] >= min_size_) {
+         avail.push_back(it);
+      }
+   }
+
+   for (auto rit = blk.end() - 1; rit > blk.begin(); --rit) {
+      size_t id = static_cast<size_t>(std::distance(blk.begin(), rit));
+      auto& [rsize, loc, _0, _00] = *rit;
+
+      size_t bid = loc;
+      if (rsize <= max_free_) {
+         for (auto itit = avail.begin(); itit != avail.end() && *itit < rit; ++itit) {
+            auto& [size, _1, off, free] = **itit;
+
+            if (free >= rsize) {
+               bool rescan = free == max_free_;
+               bid = off;
+               free -= rsize;
+               off += rsize;
+               if (free < min_size_) {
+                  avail.erase(itit);
+               }
+               if (rescan) {
+                  max_free_ = max_free(std::ranges::subrange(blk.begin(), rit));
+               }
+               break;
+            }
+         }
+      }
+
+      size_t summa = (bid+rsize) * (bid+rsize-1) / 2 -
+         bid * (bid-1) / 2;
+      csum += summa * id;
+
+      if (rsize == min_size_) {
+         size_t ms = min_size(std::ranges::subrange(blk.begin(), rit));
+         if (ms > min_size_) {
+            min_size_ = ms;
+            std::erase_if(avail, [=](auto it) {
+               return (*it)[3] < min_size_;
+            });
+         }
+      }
+   }
+
+   return csum;
+}
+
+}
+
+int main() {
+   static_assert(aoc::defrag() == 6307275788409);
+   static_assert(aoc::compress() == 6327174563252);
+}
